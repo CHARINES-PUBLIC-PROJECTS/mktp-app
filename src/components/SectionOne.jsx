503517d11f3613846ad19fc504877bc6c4d4da31
Charles Rodrigues <charles.rodrigues@dsop.com.br>
2024-12-26 21:29:25 -0300

    Hello World com as cores da DSOP

---
+++ src/components/SectionOne.jsx
@@ -0,0 +1,11 @@
+function SectionOne() {
+  return (
+    <section style={{ backgroundColor: '#43B02A', color: '#fff', padding: '20px', textAlign: 'center' }}>
+      <h2>Seção 1</h2>
+      <p>Conteúdo da primeira seção.</p>
+      <strong>Hello Workd</strong>
+    </section>
+  );
+}
+
+export default SectionOne;
